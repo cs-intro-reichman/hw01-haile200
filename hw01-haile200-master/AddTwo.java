edeec8bc1020cd0bca65ee6950d37d53b150dd35
haile200 <haile.tasma@post.runi.ac.il>
2023-12-23 18:04:42 +0200

    Add awesome code for Homework 1

---
+++ hw01-haile200-master/AddTwo.java
@@ -0,0 +1,12 @@
+/*
+ * Adds two given integers and prints the result in a fancy way.
+ */
+public class AddTwo {
+	public static void main(String[] args) {
+		String a=(args[0]);
+		String b=(args[1]);
+		System.out.println(a+"+"+b+"="+(a+b));
+
+
+	}
+}
