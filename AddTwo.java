5b9832e9dca97257947a8c5723dad006bdba6515
Haile <haile.tasma@runi.ac.il>
2023-12-23 22:51:06 +0200

    try again

--- AddTwo.java
+++ AddTwo.java
@@ -3,8 +3,8 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		int a= 3;//Integer.parseInt(args[0]);
-		int b= 5;//Integer.parseInt(args[1]);
+		int a= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
 		System.out.println(a + " + " + b + " = " + (a+b));
 
 
