14d9beae1f9a70d89d55c009e7be07be0accba36
Haile <haile.tasma@runi.ac.il>
2023-12-24 18:54:57 +0200

    try #5

--- GenThree.java
+++ GenThree.java
@@ -6,10 +6,10 @@ public class GenThree {
 	public static void main(String[] args) {
 		int lowBound= Integer.parseInt(args[0]);
 		int upBound=  Integer.parseInt(args[1]);
-		double range= (lowBound-upBound)+lowBound;
-		int rand1= (int) (Math.random ()*range)+lowBound;
-		int rand2= (int) (Math.random ()*range)+lowBound;
-		int rand3= (int) (Math.random ()*range)+lowBound;
+		double range= (lowBound-upBound+1);
+		int rand1= (int) (Math.random ()*range);
+		int rand2= (int) (Math.random ()*range);
+		int rand3= (int) (Math.random ()*range);
 		int minimal= Math.min(rand3, rand2);
 		minimal= Math.min(minimal, rand1);
 		System.out.println(rand1+"\n"+rand2+"\n"+rand3+"\n"+
